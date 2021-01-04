4e8ad706917049b741096a3449ee0d3541008c66
彭潇懿80280648 <checkpeng@cmbchina.com>
2021-01-04 13:59:25 +0800

    a

---
+++ src/com/checkpeng/leetcode/easy/最后一个单词的长度.java
@@ -0,0 +1,4 @@
+package com.checkpeng.leetcode.easy;
+
+public class 最后一个单词的长度 {
+}
---
+++ src/com/checkpeng/leetcode/middle/Z字形变换.java
@@ -0,0 +1,21 @@
+package com.checkpeng.leetcode.middle;
+
+public class Z字形变换 {
+
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        StringBuilder result = new StringBuilder();
+        int n = s.length();
+        int cycleLen = 2 * numRows - 2;// 一个Z字周期
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) {
+                result.append(s.charAt(j + i));
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+                    result.append(s.charAt(j + cycleLen - i));
+            }
+        }
+        return result.toString();
+    }
+}
