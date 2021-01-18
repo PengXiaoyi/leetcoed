dc2d7a534d75c0e0885020c7c7ad09fb64617291
Peng XiaoYi <checkpeng@cmbchina.com>
2021-01-18 13:01:50 +0800

    1

--- src/com/checkpeng/leetcode/Solution.java
+++ src/com/checkpeng/leetcode/Solution.java
@@ -6,41 +6,29 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String[] a = {"cat","banana","dog","nana","walk","walker","dogwalker"};
-        solution.longestWord(a);
+        String[] a = {"cat", "banana", "dog", "nana", "walk", "walker", "dogwalker"};
+
     }
 
-    public String longestWord(String[] words) {
-        Arrays.sort(words, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if (o1.length() == o2.length()) {
-                    return o1.compareTo(o2);
-                } else {
-                    return o2.length()-o1.length();
-                }
-            }
-        });
-        Set<String> set = new HashSet<String>(Arrays.asList(words));
-        for (String word : words) {
-            set.remove(word);
-            if (find(set, word)) {
-                return word;
-            }
+    public int closestValue(TreeNode root, double target) {
+        int a;
+        int b;
+        a = root.val;
+        while (root != null) {
         }
-        return "";
+        return 0;
     }
 
-    public boolean find(Set<String> set, String word) {
-        if (word.length() == 0) {
-            return true;
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
         }
-        for (int i = 0; i < word.length(); i++) {
-            if (set.contains(word.substring(0, i + 1))&&find(set,word.substring(i+1))){
-                return true;
-            }
-        }
-        return false;
     }
+
 }
 
