e642cec0cf6a3b5a5518d72f789f3fc919c681ef
PengXiaoyi <825138600@qq.com>
2021-02-28 21:34:02 +0800

    初始化

--- src/com/checkpeng/nowcoder/找出最小的K个数.java
+++ src/com/checkpeng/nowcoder/找出最小的K个数.java
@@ -1,30 +1,43 @@
 package com.checkpeng.nowcoder;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class 找出最小的K个数 {
 
-    public ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
-        getIndex(input, 0, input.length - 1, k);
-
+    public static void main(String[] args) {
+        int[] arr = new int[]{4, 5, 1, 6, 2, 7, 3, 8};
+        System.out.println(GetLeastNumbers_Solution(arr, 4));
     }
 
-    public int getIndex(int[] arr, int start, int end, int k) {
+    public static ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
+        if (k > input.length || k < 0 || input.length == 0) {
+            return new ArrayList<>();
+        }
+        getIndex(input, 0, input.length - 1, k);
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            result.add(input[i]);
+        }
+        return result;
+    }
+
+    public static int getIndex(int[] arr, int start, int end, int k) {
         int m = start;
         int n = end;
         int temp = arr[m];
         while (m < n) {
-            while (arr[n] >= temp) {
+            while (m < n && arr[n] >= temp) {
                 n--;
             }
             arr[m] = arr[n];
-            while (arr[m] <= temp) {
+            while (m < n && arr[m] <= temp) {
                 m++;
             }
             arr[n] = arr[m];
         }
         arr[m] = temp;
-        if (m == k || m == k + 1) {
+        if (m == k - 1 || m == k) {
             return k;
         }
         if (m > k) {
