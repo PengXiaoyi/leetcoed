2b208dfc69d348fb9ffe4135c8165039c4917b4d
PengXiaoyi <825138600@qq.com>
2021-03-15 18:11:45 +0800

    初始化

--- src/com/checkpeng/javademo/algorithm/sort/HeapSort2.java
+++ src/com/checkpeng/javademo/algorithm/sort/HeapSort2.java
@@ -1,12 +1,14 @@
 package com.checkpeng.javademo.algorithm.sort;
 
+import java.util.PriorityQueue;
+
 /**
  * 堆排序（Heapsort）是指利用堆这种数据结构所设计的一种排序算法。堆积是一个近似完全二叉树的结构，并同时满足堆积的性质：即子结点的键值或索引总是小于（或者大于）它的父节点。
  * 将初始待排序关键字序列(R1,R2….Rn)构建成大顶堆，此堆为初始的无序区；
  * 将堆顶元素R[1]与最后一个元素R[n]交换，此时得到新的无序区(R1,R2,……Rn-1)和新的有序区(Rn),且满足R[1,2…n-1]<=R[n]；
  * 由于交换后新的堆顶R[1]可能违反堆的性质，因此需要对当前无序区(R1,R2,……Rn-1)调整为新堆，然后再次将R[1]与无序区最后一个元素交换，得到新的无序区(R1,R2….Rn-2)和新的有序区(Rn-1,Rn)。不断重复此过程直到有序区的元素个数为n-1，则整个排序过程完成。
  */
-public class HeapSort {
+public class HeapSort2 {
 
     public static void main(String[] args) {
         int arr[] = {9, 7, 6, 8, 5, 4, 3, 2, 1};
@@ -18,46 +20,21 @@ public class HeapSort {
 
         int len = arr.length;
 
-        // 构建大顶堆
-        buildMaxHeap(arr, len);
+        PriorityQueue<Integer> maxHeap = buildMaxHeap(arr);
 
-        // 每次从大顶堆把最大的元素拿出去，放到队列末
-        for (int i = len - 1; i > 0; i--) {
-            swap(arr, 0, i);
-            len--;
-            heapify(arr, 0, len);
+        for (int i = arr.length - 1; i >= 0; i--) {
+            arr[i] = maxHeap.poll();
         }
         return arr;
     }
 
-    private static void buildMaxHeap(int[] arr, int len) {
-        for (int i = (int) Math.floor(len / 2); i >= 0; i--) {
-            heapify(arr, i, len);
+    private static PriorityQueue<Integer> buildMaxHeap(int[] arr) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>(arr.length, (num1, num2) -> {
+            return num2 - num1;
+        });
+        for (int i = 0; i < arr.length; i++) {
+            heap.add(arr[i]);
         }
-    }
-
-    private static void heapify(int[] arr, int i, int len) {
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
-        int largest = i;
-
-        if (left < len && arr[left] > arr[largest]) {
-            largest = left;
-        }
-
-        if (right < len && arr[right] > arr[largest]) {
-            largest = right;
-        }
-
-        if (largest != i) {
-            swap(arr, i, largest);
-            heapify(arr, largest, len);
-        }
-    }
-
-    private static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        return heap;
     }
 }
